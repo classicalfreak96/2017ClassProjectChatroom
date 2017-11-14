05bcced4f71646121a2de8e9814442ee61dd1633
Harrison Lu <harrisonlu@wustl.edu>
2017-11-13 21:33:15 -0600

    chat different across rooms, cannot enter same chat room twice or empty chat room, click on message to either ban or kick user from chatgroup

--- chat-server2.js
+++ chat-server2.js
@@ -1,30 +1,30 @@
 // Require the packages we will use:
 var http = require("http"),
-	socketio = require("socket.io"),
-	fs = require("fs");
+socketio = require("socket.io"),
+fs = require("fs");
+
+var usernames = [];
+var chatrooms = [];
 
 // Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
 var app = http.createServer(function(req, resp){
 	// This callback runs when a new connection is made to our HTTP server.
 	fs.readFile("mainPage.html", function(err, data){
 		// This callback runs when the client.html file has been read from the filesystem.
-		console.log("!!!!!!!");
-			console.log(req.url);
 		if (req.url =='/chatroom.css'){
-			console.log("in CSS2");
 			fs.readFile("chatroom.css", function(err, data){
 				if (err) console.log(err);
-        resp.writeHead(200, {'Content-Type': 'text/css'});
-        resp.write(data);
-        resp.end();
+				resp.writeHead(200, {'Content-Type': 'text/css'});
+				resp.write(data);
+				resp.end();
 			});
 			
 		}
 		else{
-		if(err) return resp.writeHead(500);
-		resp.writeHead(200);
-		resp.write(data);
-		resp.end();
+			if(err) return resp.writeHead(500);
+			resp.writeHead(200);
+			resp.write(data);
+			resp.end();
 		}
 	});
 });
@@ -34,13 +34,39 @@ app.listen(3456);
 var io = socketio.listen(app);
 io.sockets.on("connection", function(socket){
 	// This callback runs when a new Socket.IO connection is established.
-	
-	socket.on('message_to_server', function(data) {
-		// This callback runs when the server receives a new message from the client.
-		
-		console.log("message: "+data["message"]); // log it to the Node.JS output
-		io.sockets.emit("message_to_client",{message:data["message"] }) // broadcast the message to other users
+	io.sockets.emit("updateRooms", chatrooms);
+	socket.on('adduser', function(username){
+		// store the username in the socket session for this client
+		socket.username = username;
 	});
+	socket.on('message_to_server', function(data) {
+		io.sockets.in(socket.room).emit("message_to_client", socket.username, {message:data["message"] }) // broadcast the message to other users
+	});
+	socket.on('addChatroom', function(chatName) {
+		var match = false;
+		chatrooms.forEach(function(room){
+			if (room == chatName) {
+				match = true;
+			};
+		});
+		if (match) {
+			io.sockets.emit("usageMessage", "chatroom already exists");
+		}
+		else if (chatName == "") {
+			io.sockets.emit("usageMessage", "you cannot type in an empty value");
+		}
+		else {
+				console.log(chatrooms);
+				io.sockets.emit("confirmAddChatroom", chatName);
+				chatrooms.push(chatName);
+				io.sockets.emit("updateRooms", chatrooms);
+			};
+		});
+	socket.on('switchRoom', function(chatName) {
+		socket.leave(socket.room);
+		socket.join(chatName);
+		socket.room = chatName;
+		console.log(socket.username + " is in " + socket.room);
+	});
+
 });
-
-
